52a9c7d20aadda120b7d7c64b68e2b7fa8c41085
passionwang <passioneday@126.com>
2022-10-23 14:05:27 +0800

    first run

---
+++ examples/test.py
@@ -0,0 +1,16 @@
+import glob
+import os
+import sys
+import ftrace
+from ftrace import Ftrace, Interval
+
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser(description='Per-core frequencies')
+    # parser.add_argument('-f', '--file', dest='file',
+    #                     help='File to parse')
+    # args = parser.parse_args()
+    #
+    # trace = Ftrace(args.file)
+    trace = Ftrace(r"/Users/passionwang/code/adb-tool/platform-tools 2/systrace/20221023-112035.html")
+    print "num_janks: " + str(trace.android.num_janks(interval=None))
+    print 'Average FPS: ' + str(trace.android.jankrate(interval=None))
